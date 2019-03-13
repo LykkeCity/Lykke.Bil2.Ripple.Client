7835529caf09de5a7d038bacb07862401d98b687
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2019-03-13 16:05:36 +0300

    Added aux method to throw if request indicates error

--- src/Lykke.Bil2.Ripple.Client/RippleResponseResultBase.cs
+++ src/Lykke.Bil2.Ripple.Client/RippleResponseResultBase.cs
@@ -19,5 +19,22 @@ namespace Lykke.Bil2.Ripple.Client
         /// </summary>
         [JsonProperty("error")]
         public string Error { get; set; }
+
+        /// <summary>
+        /// A copy of the request that prompted this error, in JSON format.
+        /// </summary>
+        [JsonProperty("request")]
+        public object Request { get; set; }
+
+        /// <summary>
+        /// Throws <see cref="RippleResponseResultErrorException"/> if <see cref="Status"/> indicates error.
+        /// </summary>
+        public void ThrowIfError()
+        {
+            if (Status == "error")
+            {
+                throw new RippleResponseResultErrorException(Error, Request);
+            }
+        }
     }
 }
---
+++ src/Lykke.Bil2.Ripple.Client/RippleResponseResultErrorException.cs
@@ -0,0 +1,18 @@
+using System;
+using Newtonsoft.Json;
+
+namespace Lykke.Bil2.Ripple.Client
+{
+    public class RippleResponseResultErrorException : Exception
+    {
+        /// <summary>
+        /// Initializes new instance of <see cref="RippleResponseResultErrorException"/>.
+        /// </summary>
+        /// <param name="error">A unique code for the type of error that occurred.</param>
+        /// <param name="request">A copy of the request that prompted this error.</param>
+        public RippleResponseResultErrorException(string error, object request) :
+            base($"Ripple request failed. Error: {error}. Request: {JsonConvert.SerializeObject(request)}")
+        {
+        }
+    }
+}
